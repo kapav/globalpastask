c69cd7c717632ba116861d422cb46465c377f442
Александр Кочкин <cybyn@yandex.ru>
2025-03-23 21:46:33 +0400

    GPT-11_Добавление-пагинации

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
 import { DialogService, DynamicDialogConfig, DynamicDialogModule, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { BookService } from '../../services/book/book.service';
 import { Book } from '../../interfaces/book.interface';
@@ -10,14 +11,12 @@ import { EditComponent } from '../edit/edit.component';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardModule, ButtonModule, TableModule, DynamicDialogModule],
+  imports: [CardModule, ButtonModule, TableModule, DynamicDialogModule, PaginatorModule],
   providers: [DialogService, DynamicDialogConfig],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent implements OnInit , OnDestroy {
-  loading: boolean = false;
-
   constructor(
     public dialogService: DialogService,
     public config: DynamicDialogConfig,
@@ -31,23 +30,6 @@ export class ListComponent implements OnInit , OnDestroy {
 
   get books(): Book[] {
     return this.bookService.getAll();
-  }
-
-  add() {
-    const isAdd = true;
-    this.ref = this.dialogService.open(EditComponent, {
-      header: 'Добавление книги',
-      width: '50vw',
-      modal: true,
-      contentStyle: { overflow: 'auto' },
-      breakpoints: {
-          '960px': '75vw',
-          '640px': '90vw'
-      },
-      data: {
-        isAdd,
-      },
-    });
   }
 
   edit(isAdd: boolean, book?: Book): void {
